43815079211d9e436c45984c2d5f8be50db88a27
Mitchell <198377+mitchell-fyi@users.noreply.github.com>
2023-02-27 21:12:51 +0000

    split up controller tests

--- test/controllers/accounts_controller_test.rb
+++ test/controllers/accounts_controller_test.rb
@@ -12,19 +12,23 @@ class AccountsControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
-  test 'should get new' do
+  test 'should redirect to sign up when trying to get new when logged out' do
     get new_account_url
     assert_redirected_to new_user_registration_url
+  end
 
+  test 'should get new when any user is logged in' do
     sign_in users(:example)
     get new_account_url
     assert_response :success
   end
 
-  test 'should create account' do
+  test 'should redirect to sign up when trying to create when logged out' do
     post accounts_url, params: { account: { domain: 'example123.com' } }
     assert_redirected_to new_user_registration_url
+  end
 
+  test 'should create account when any user is logged in' do
     sign_in users(:example)
     assert_difference('Account.count') do
       post accounts_url, params: { account: { domain: 'example123.com' } }
@@ -41,14 +45,19 @@ class AccountsControllerTest < ActionDispatch::IntegrationTest
     assert_redirected_to @account.url
   end
 
-  test 'should get edit' do
+  test 'should redirect to sign up when trying to edit when logged out' do
     get edit_account_url(@account)
     assert_redirected_to new_user_registration_url
+  end
 
+  test 'should redirect to root when trying to edit without permission' do
     sign_in users(:example)
     get edit_account_url(@account)
     assert_redirected_to root_url
+  end
 
+  test 'should get edit with permission' do
+    sign_in users(:example)
     users(:example).add_role(:staff, @account)
     get edit_account_url(@account)
     assert_response :success
@@ -59,14 +68,19 @@ class AccountsControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
-  test 'should update account' do
+  test 'should redirect to sign up when trying to update when logged out' do
     patch account_url(@account), params: { account: { name: 'example' } }
     assert_redirected_to new_user_registration_url
+  end
 
+  test 'should redirect to root when trying to update without permission' do
     sign_in users(:example)
     patch account_url(@account), params: { account: { name: 'example' } }
     assert_redirected_to root_url
+  end
 
+  test 'should update with permission' do
+    sign_in users(:example)
     users(:example).add_role(:staff, @account)
     patch account_url(@account), params: { account: { name: 'example' } }
     assert_redirected_to edit_account_url(@account)
@@ -77,10 +91,12 @@ class AccountsControllerTest < ActionDispatch::IntegrationTest
     assert_redirected_to edit_account_url(@account)
   end
 
-  test 'should destroy account' do
+  test 'should redirect to sign up when trying to destroy when logged out' do
     delete account_url(@account)
     assert_redirected_to new_user_registration_url
+  end
 
+  test 'should redirect to root when trying to destroy without permission' do
     sign_in users(:example)
     assert_difference('Account.count', 0) do
       delete account_url(@account)
@@ -92,12 +108,15 @@ class AccountsControllerTest < ActionDispatch::IntegrationTest
       delete account_url(@account)
     end
     assert_redirected_to root_url
+  end
 
+  test 'should destroy with permission' do
+    sign_in users(:example)
     users(:example).add_role(:owner, @account)
     assert_difference('Account.count', -1) do
       delete account_url(@account)
     end
 
-    assert_redirected_to root_url(subdomain: false)
+    assert_redirected_to root_url
   end
 end
